e4fa79ca9359550aea4d8660a39c5796151f2adf
Boyko Mihail <mikhail.boyko@touchin.ru>
2020-02-28 13:54:44 +0300

    update api 2

--- api.py
+++ api.py
@@ -32,8 +32,8 @@ api = MDApiConnector(
 
 fapi = FundamentalApi()
 
-storage = DataStorage(api)
-storage.start()
+#storage = DataStorage(api)
+#storage.start()
 
 # Импортируем подмодули Flask для запуска веб-сервиса.
 from flask import Flask, request
