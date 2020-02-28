84d710485261df93f9ed7ceffd99f536fe673d0b
Boyko Mihail <mikhail.boyko@touchin.ru>
2020-02-28 13:59:26 +0300

    add all

--- api.py
+++ api.py
@@ -10,6 +10,38 @@ import logging
 from flask import Flask, request
 app = Flask(__name__)
 
+import re
+from sys import path
+from decimal import Decimal
+
+from configparser import ConfigParser
+
+from mdapi import DataStorage, MDApiConnector
+from fundamental import FundamentalApi
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+# read config
+config = ConfigParser()
+config.read_file(open('config.ini'))
+
+# create connectors to API
+api = MDApiConnector(
+    client_id=config['API']['client_id'],
+    app_id=config['API']['app_id'],
+    key=config['API']['shared_key']
+)
+
+fapi = FundamentalApi()
+
+# start stock storage
+storage = DataStorage(api)
+storage.start()
+
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -102,3 +134,60 @@ def get_suggests(user_id):
         })
 
     return suggests
+
+# Welcome message
+def start(bot, update):
+    msg = "Привет, {user_name}! \n\n" + \
+    "Меня можно спросить об акциях фондового рынка США \n" + \
+    "и я покажу их оценку P/E и текущую цену. \n\n" + \
+    "Например: расскажи об AAPL или NVDA"
+
+    # Send the message
+    bot.send_message(chat_id=update.message.chat_id,
+                     text=msg.format(
+                         user_name=update.message.from_user.first_name,
+                         bot_name=bot.name))
+
+@run_async
+def process(bot, update):
+    if update.message.text.find("брокера посоветуешь") > 0:
+        update.message.reply_text("Лично я рекомендую EXANTE!")
+        return
+
+    tickers = re.findall(r'[A-Z]{1,4}', update.message.text)
+
+    msg = ""
+    for ticker in tickers:
+        stock = storage.stocks.get(ticker)
+        if not stock: continue
+
+        eps = fapi.request(ticker).get('EarningsShare')
+        if not eps:
+            logger.warning("Can't fetch EPS for {}".format(ticker))
+            continue
+
+        price = api.get_last_ohlc_bar(stock['id'])
+        ratio = Decimal("%.4f" % price['close']) / Decimal(eps)
+
+        msg += "{ticker} ({name}, {exchange}): EPS {eps}, P/E {ratio}, цена ${price} \n".format(
+            ticker = ticker,
+            name = stock['description'],
+            exchange = stock['exchange'],
+            ratio = "%.2f" % ratio,
+            price = price['close'],
+            eps = eps
+        )
+
+    if not msg:
+        msg = "Не удалось получить данные по тикерам из запроса :(\n" +\
+              "Попробуйте спросить о чем-то популярном, вроде GOOG или AAPL."
+
+    bot.send_message(chat_id=update.message.chat_id, text=msg)
+
+def main():
+    # Add handlers to dispatcher
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(MessageHandler(Filters.text, process))
+
+    up.start_polling()
+    up.idle()
