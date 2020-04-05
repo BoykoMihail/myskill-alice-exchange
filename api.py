01b8e00200b24264709e23b998e8f60768acc1f1
Boyko Mihail <mikhail.boyko@touchin.ru>
2020-04-05 17:44:09 +0300

    Add data base and parsing strigs

--- api.py
+++ api.py
@@ -7,9 +7,14 @@ import re
 import json
 from yahoo_fin import stock_info as si
 import requests
+import random
+import string
 
 import firebase_admin
 from firebase_admin import credentials, firestore
+from names_translator import Transliterator
+import difflib
+import itertools
 # Импортируем подмодули Flask для запуска веб-сервиса.
 from flask import Flask, request
 app = Flask(__name__)
@@ -35,6 +40,11 @@ def main():
         ensure_ascii=False,
         indent=2
     )
+    
+def randomString(stringLength=10):
+    """Generate a random string of fixed length """
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
 
 def get_symbol(symbol):
     url = "http://d.yimg.com/autoc.finance.yahoo.com/autoc?query={}&region=1&lang=en".format(symbol)
@@ -54,6 +64,21 @@ def get_exchDisp(symbol):
         if x['symbol'] == symbol:
             return x['exchDisp']
             
+def isEnglish(s):
+    try:
+        s.encode(encoding='utf-8').decode('ascii')
+    except UnicodeDecodeError:
+        return False
+    else:
+        return True
+        
+def listToString(s,t):
+    str1 = ""
+    for ele in s:
+        str1 += ele
+        str1 += t
+    return str1[:-1]
+            
 # Функция для непосредственной обработки диалога.
 def handle_dialog(req, res):
     user_id = req['session']['user_id']
@@ -65,88 +90,96 @@ def handle_dialog(req, res):
         msg = " Привет! \n\n" + \
         "Меня можно спросить об акциях фондового рынка США \n" + \
         "Например: расскажи об AAPL или NVDA"
+
+        res['response']['text'] = msg
+        return
+    
+    list = ["ПОМОЩЬ", "ЧТО ТЫ УМЕЕШЬ?", "ЧТО ТЫ УМЕЕШЬ", "ЧТО УМЕЕШЬ", "ЧТО УМЕЕШЬ?"]
+    
+    if req['request']['original_utterance'].upper() in list:
+        # Помощь
         
-#        # TO DO
+        msg = "Я умею информировать тебя о котировках на акции фондового рынка США \n" + \
+            "Например Ты можешь сказать: расскажи об AAPL или NVDA\n" + \
+            "А я Тебе отвечу: AAPL (Apple Inc., NASDAQ): цена $242.2100067138672"
 
+        res['response']['text'] = msg
+        return
+    
+    tickers = re.findall(r'[A-Z]{1,4}', req['request']['original_utterance'])
+    tr = Transliterator()
+    textCurrent = req['request']['original_utterance']
+    textCurrent = textCurrent.replace(',', ' ')
+    textCurrent = textCurrent.replace('.', '')
+    textArray = textCurrent.split(' ')
+    textTranslate = []
+    temp = []
+    
+    for t in textArray:
+        translate = ""
+        for val in tr.transliterate(t, "", ""):
+            if isEnglish(val):
+                translate = val
+                break
+        textTranslate.append(translate)
+        temp.append(translate)
+    
 
-        cred = credentials.Certificate("serviceAccountKey.json")
-        firebase_admin.initialize_app(cred)
+    if len(temp) > 2:
+        for item in itertools.combinations(temp, 3):
+            textTranslate.append(' '.join(item))
+            textTranslate.append('-'.join(item))
+    if len(temp) > 1:
+        for item in itertools.combinations(temp, 2):
+            textTranslate.append(' '.join(item))
+            textTranslate.append('-'.join(item))
+    
+    cred = credentials.Certificate("serviceAccountKey.json")
+    firebaseApp = firebase_admin.initialize_app(cred)
+    datab = firestore.client()
 
-        datab = firestore.client()
+    try:
+
         usersref = datab.collection(u'tickers')
         docs = usersref.stream()
-
+        
+        tickerCompanyName = []
+        tickerName = []
+        tickerNameMap = {}
         for doc in docs:
-            msg = msg + '{} : {}'.format(doc.id,doc.to_dict())
+            tickerCompanyName.append(doc.to_dict()['EnglishName'])
+            tickerNameMap[doc.to_dict()['EnglishName']] = doc.to_dict()['Ticker']
+        
+        realNames = []
+        for t in textTranslate:
+            findName = difflib.get_close_matches(t, tickerCompanyName)
+            if len(findName) > 0:
+                if tickerNameMap[findName[0]] not in tickers :
+                    tickers.append(tickerNameMap[findName[0]])
 
-        res['response']['text'] = msg
-        return
-        
-    if req['request']['original_utterance'].upper() == "ПОМОЩЬ":
-        # Помощь
-        
-        msg = "Меня можно спросить об акциях фондового рынка США \n" + \
-            "Например: расскажи об AAPL или NVDA"
+        firebase_admin.delete_app(firebaseApp)
 
-        res['response']['text'] = msg
-        return
-        
-    if req['request']['original_utterance'].upper() == "ЧТО ТЫ УМЕЕШЬ?":
-        # Помощь
-        
-        msg = "Я умею информировать тебя о котировках на акции фондового рынка США \n" + \
-            "Например Ты можешь сказать: расскажи об AAPL или NVDA\n" + \
-            "А я Тебе отвечу: AAPL (Apple Inc., NASDAQ): цена $242.2100067138672"
 
-        res['response']['text'] = msg
-        return
-        
-    if req['request']['original_utterance'].upper() == "ЧТО ТЫ УМЕЕШЬ":
-        # Помощь
-        
-        msg = "Я умею информировать тебя о котировках на акции фондового рынка США \n" + \
-            "Например Ты можешь сказать: расскажи об AAPL или NVDA\n" + \
-            "А я Тебе отвечу: AAPL (Apple Inc., NASDAQ): цена $242.2100067138672"
+    except Exception as e:
+        firebase_admin.delete_app(firebaseApp)
+        print(e)
+        print(u'No such document!')
 
-        res['response']['text'] = msg
-        return
-        
-    if req['request']['original_utterance'].upper() == "ЧТО УМЕЕШЬ":
-        # Помощь
-        
-        msg = "Я умею информировать тебя о котировках на акции фондового рынка США \n" + \
-            "Например Ты можешь сказать: расскажи об AAPL или NVDA\n" + \
-            "А я Тебе отвечу: AAPL (Apple Inc., NASDAQ): цена $242.2100067138672"
-
-        res['response']['text'] = msg
-        return
-        
-    if req['request']['original_utterance'].upper() == "ЧТО УМЕЕШЬ?":
-        # Помощь
-        
-        msg = "Я умею информировать тебя о котировках на акции фондового рынка США \n" + \
-            "Например Ты можешь сказать: расскажи об AAPL или NVDA\n" + \
-            "А я Тебе отвечу: AAPL (Apple Inc., NASDAQ): цена $242.2100067138672"
-
-        res['response']['text'] = msg
-        return
-        
-    
-        
-    tickers = re.findall(r'[A-Z]{1,4}', req['request']['original_utterance'])
-    msg = "Извините, по таким тикерам ничего не найдено. Попробуйте еще раз!"
-    
-    for ticker in tickers:
+    msg = ""
+    if len(tickers) == 0:
+        msg = "Извините, по таким тикерам ничего не найдено. Попробуйте еще раз!"
+    else:
         msg = ""
-        name = get_symbol(ticker)
-        price = str(si.get_live_price(ticker))
-        ex_desc = get_exchDisp(ticker)
-        msg += "{ticker} ({name}, {exchange}): цена ${price} \n".format(
-            ticker = ticker,
-            name = name,
-            exchange = ex_desc,
-            price = price
-        )
+        for ticker in tickers:
+            name = get_symbol(ticker)
+            price = str(si.get_live_price(ticker))
+            ex_desc = get_exchDisp(ticker)
+            msg += "{ticker} ({name}, {exchange}): цена ${price} \n".format(
+                ticker = ticker,
+                name = name,
+                exchange = ex_desc,
+                price = price
+            )
         
     res['response']['text'] = msg
     return
